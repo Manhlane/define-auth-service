5e3ccc2e30b315001c1330d8625a621994b09b67
manhlane <mamabolo.m32@gmail.com>
2025-06-30 23:44:57 +0200

    Updated user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
+import { Session } from 'src/session/entities/session.entity';
 import { BaseEntity } from 'src/common/entities/base.entity/base.entity';
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, OneToMany } from 'typeorm';
 
 @Entity('users')
 export class User extends BaseEntity {
@@ -11,4 +12,13 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @Column({ default: false })
+  isVerified: boolean;
+
+  @Column('simple-array', { default: 'user' })
+  roles: string[];
+
+  @OneToMany(() => Session, (session) => session.user)
+  sessions: Session[];
 }
