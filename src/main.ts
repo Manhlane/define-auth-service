a3b7fc90f8b8f9ba0a5bbba2830b267adde5c553
manhlane <mamabolo.m32@gmail.com>
2026-01-31 21:59:02 +0200

    debug allowed origins

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,12 @@ async function bootstrap() {
 
   // ✅ CORS for your Next.js app on :3002 (and optional env origin)
   const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3003',
     process.env.FRONTEND_ORIGIN ?? '',
   ]
     .filter(Boolean)
     .filter((value, index, self) => self.indexOf(value) === index);
 
+    console.log(process.env.FRONTEND_ORIGIN)
   app.enableCors({
     origin: allowedOrigins, // exact origins, not '*'
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
