8026c98e9be443735833bdf30e93a929f0e029aa
manhlane <mamabolo.m32@gmail.com>
2026-06-08 22:18:15 +0200

    changed swagger path

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('dfn/a/swagger', app, document);
 
   await app.listen(process.env.PORT, '0.0.0.0');
   console.log(`Auth API running on ${await app.getUrl()}`);
